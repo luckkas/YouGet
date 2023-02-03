8f5f6ba38fe38e5d1b9ba1e3e084f43f929894c2
luckkas <luckasmelo.dev@gmail.com>
2023-02-03 20:04:01 -0300

    layout: add a simple search bar and button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-   <h1>Hello World</h1>
+   <v-app>
+      <v-main>
+         <v-container class="w-100 justify-center">
+            <v-row dense>
+               <v-col><v-text-field height="2"></v-text-field></v-col>
+               <v-col
+                  ><v-btn height="55" variant="outlined"> Search </v-btn></v-col
+               >
+            </v-row>
+         </v-container>
+      </v-main>
+   </v-app>
 </template>
